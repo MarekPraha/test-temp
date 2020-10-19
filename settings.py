509a8584b924909d46480ae9ca940a7fe4ebc2c0
unknown <krystof.dusek.cz@gmail.com>
2020-10-19 16:37:35 +0200

    SDA

--- settings.py
+++ settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
